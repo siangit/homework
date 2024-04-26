b199576ac437e100140b49dd6e0fcc56737fd439
https://github.com/siangit/homework.git <ljin6956@gmail.com>
2024-04-26 21:03:11 +0900

    course video 3 (api) done

--- oz-backend-flask/Part1/05.JinjaTemplate/app.py
+++ oz-backend-flask/Part1/05.JinjaTemplate/app.py
@@ -1,21 +1,24 @@
 from flask import Flask, render_template
-
+#it's easy to create the web page through python
+#render_template is smoothe to make the html in flask
+#since i called out  the flask class, i need to initialize the variables.
 app = Flask(__name__)
 
+#route is for the user who want to move to the exact url directly with the url address
 @app.route("/")
 def index():
     data = {
         'title':'Flask Jinja Template',
         'user':'sian',
-        'is_admint':True,
+        # if it is true, that means that the user has the admin previliage
+        'is_admin':True,
         'items_list': ["Item1","Item2","Item3"]
     }
-
-    title = 'flask Jinja Template'
-
     # 1. rendering할 html 파일명 입력
     # 2. html로 넘겨줄 데이터 입력
     return render_template('index.html',data=data)
+
+
 
 @app.route("/users")
 def users():
