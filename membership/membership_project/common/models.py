3c3c1cc1c30a6811b8f3b09645a7cb162fea5bd3
https://github.com/siangit/homework.git <ljin6956@gmail.com>
2024-06-05 01:04:56 +0900

    django hw2 fianl

---
+++ membership/membership_project/common/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class CommonModel(models.Model):
+    create_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
