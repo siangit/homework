d86e791d6915045a97d4c55b997392772170153e
https://github.com/siangit/homework.git <ljin6956@gmail.com>
2024-06-05 01:37:03 +0900

    hw4-1 done final

--- membership/membership_project/membership_project/settings.py
+++ membership/membership_project/membership_project/settings.py
@@ -29,18 +29,23 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
-INSTALLED_APPS = [
+DJANGO_SYSTEM_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+]
+
+CUSTOM_USER_APPS = [
     'accounts.apps.AccountsConfig',
     'common.apps.CommonConfig',
     'addresses.apps.AddressesConfig',
+    'rest_framework',
 ]
+
+INSTALLED_APPS = DJANGO_SYSTEM_APPS + CUSTOM_USER_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
