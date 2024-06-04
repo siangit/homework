ddda94d826dd7ce56935a95639c59410dec7ba58
https://github.com/siangit/homework.git <ljin6956@gmail.com>
2024-06-05 02:22:43 +0900

    hw4 2-2 done final

---
+++ membership/membership_project/addresses/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework.serializers import ModelSerializer
+from .models import Address
+
+class AddressSerializer(ModelSerializer):
+    class Meta:
+        model = Address
+        fields= "__all__"
+        depth = 1
+
+class AddressListSerializer(ModelSerializer):
+    class Meta:
+        model = Address
+        fields = ("user", "street", "city", "state", "postal_code","country")
--- membership/membership_project/addresses/views.py
+++ membership/membership_project/addresses/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import AddressSerializer
+from .models import Address
 
 # Create your views here.
+class Addresses(APIView):
+    def get(self, request):
+        addresses = Address.objects.all()
+        serializer = AddressSerializer(addresses,many=True)
+        return Response(serializer.data)
